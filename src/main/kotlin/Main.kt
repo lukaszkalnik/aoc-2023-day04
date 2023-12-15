37ea778257c584280e51359b6a8c3eb2bec058ef
Lukasz Kalnik <lukasz.kalnik@gmail.com>
2023-12-15 20:55:05 +0100

    Use sumOf

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,14 +6,14 @@ import kotlin.math.pow
 fun main() {
     val input = File("input.txt").readLines()
     val scratchcards = input.map { it.split(":")[1] }
-    val sumOfPoints = scratchcards.map { scratchcard ->
+    val sumOfPoints = scratchcards.sumOf { scratchcard ->
         val splitScratchcard = scratchcard.split("|")
         val winningNumbers = splitScratchcard[0].parseNumbers()
         val chosenNumbers = splitScratchcard[1].parseNumbers()
 
         chosenNumbers.filter { winningNumbers.contains(it) }
             .let { if (it.isEmpty()) 0 else 2.toDouble().pow(it.size - 1).toInt() }
-    }.sum()
+    }
 
     println(sumOfPoints)
 }
